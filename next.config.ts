b1db558699c489318e9d063cd5a4109a195ddf38
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 01:38:18 +0530

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/workspace-*/image/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
